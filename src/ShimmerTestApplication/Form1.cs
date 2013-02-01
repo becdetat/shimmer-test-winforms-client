daf4e9ed92a66d198f6f97b839f4ede38bb6cc2c
Ben Scott <ben@belfryimages.com.au>
2013-02-01 15:15:17 +1000

    can check local (empty) path

--- src/ShimmerTestApplication/Form1.cs
+++ src/ShimmerTestApplication/Form1.cs
@@ -6,14 +6,52 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Shimmer.Client;
+using System.Reactive.Subjects;
 
 namespace ShimmerTestApplication
 {
     public partial class Form1 : Form
     {
+        UpdateManager _updateManager;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (_updateManager != null) _updateManager.Dispose();
+            base.OnClosing(e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_updateManager != null) _updateManager.Dispose();
+
+            _updateManager = new UpdateManager(
+                //"https://raw.github.com/belfryimages/shimmer-test-winforms-client/master/update-path/",
+                @"..\..\..\..\update-path\",
+                "ShimmerTestApplication",
+                FrameworkVersion.Net40);
+
+            _updateManager.CheckForUpdate().Subscribe(updateInfo =>
+            {
+                if (updateInfo == null)
+                {
+                    MessageBox.Show("No updates found");
+                    return;
+                }
+                MessageBox.Show(string.Format("Found {0} releases to apply", updateInfo.ReleasesToApply.Count()));
+                foreach (var r in updateInfo.ReleasesToApply)
+                {
+                    MessageBox.Show(string.Format(
+                        "{0} {1}",
+                        r.PackageName,
+                        r.Version.ToString()));
+                }
+            });
         }
     }
 }
